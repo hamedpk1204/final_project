ae4bfd8abf0189b052c3b04f61480a1352d6ff2e
Hamed Pourkaveh <hamedpk1204@gmail.com>
2019-04-12 00:09:12 -0400

    add secure password and attribute to users

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.6.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
+gem 'bcrypt', '~> 3.1.7'
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'jquery-rails'
 # Use Puma as the app server
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_030001) do
+ActiveRecord::Schema.define(version: 2019_04_12_040138) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "works", force: :cascade do |t|
